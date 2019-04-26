fd1eeccfe7de2402cedc77e2ebf3af34644c483d
julie <julielerman@gmail.com>
2019-04-26 17:43:35 -0400

    clean

--- ModelTests/entrypoint-tests.sh
+++ ModelTests/entrypoint-tests.sh
@@ -3,5 +3,4 @@ set -eu -o pipefail
  >&2 echo "in entrypoint-tests file"
 echo "waiting for SQL Server to start up"
 sleep 8s
-dotnet restore ModelTests/ModelTests.csproj   
 dotnet test ModelTests/ModelTests.csproj
