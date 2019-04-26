589d89ca98ea7470e448f6611a71a027085a14bd
julie <julielerman@gmail.com>
2019-04-26 17:43:20 -0400

    clean

--- ModelTests/entrypoint-tests.sh
+++ ModelTests/entrypoint-tests.sh
@@ -3,14 +3,4 @@ set -eu -o pipefail
  >&2 echo "in entrypoint-tests file"
 echo "waiting for SQL Server to start up"
 sleep 8s
-dotnet restore ModelTests/ModelTests.csproj   
 dotnet test ModelTests/ModelTests.csproj
-# set -e
-
-# until dotnet ef database update; do
-# >&2 echo "SQL Server is starting up"
-# sleep 1
-# done
-
-# >&2 echo "SQL Server is up - executing command"
-# exec $run_cmd
