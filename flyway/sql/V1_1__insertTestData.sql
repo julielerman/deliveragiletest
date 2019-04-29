8db26b5639064034b9c14216eccef2914bb4e9e4
julie <julielerman@gmail.com>
2019-04-28 22:10:02 -0400

    updated tester data

--- flyway/sql/V1_1__insertTestData.sql
+++ flyway/sql/V1_1__insertTestData.sql
@@ -15,7 +15,7 @@ GO
 INSERT INTO [Agilistas] ([Id], [Name], [PrimaryFocusId], [TwitterHandle])
 VALUES ('5efdb55d-1205-419f-8a0b-9cc7a15f8565', N'Lisa Crispin', '167d1f6b-a93d-49e4-8a0d-e651369e018b', '@lisacrispin'),
 ('554be900-d1a4-4908-9d6a-5ef3ea05650e', N'Janet Gregory', '167d1f6b-a93d-49e4-8a0d-e651369e018b', '@janetgregoryca'),
-('f2e5fdba-daf9-43f0-8bde-9ed2562bd12a', N'Maaret Pyhäjärvi', '5f6d6f80-9f9a-469e-9036-07ecbb3971ea', '@maaretp'),
+('f2e5fdba-daf9-43f0-8bde-9ed2562bd12a', N'Melissa Eaden', '167d1f6b-a93d-49e4-8a0d-e651369e018b', '@melthetester'),
 ('83eda86f-c652-4666-ba17-db90b218a54b', N'Linda Rising', 'c5b6a0e8-e43f-4765-906f-e15e019a19d8', '@risinglinda'),
 ('70de119b-3860-42c4-8150-19c7d017c0b5', N'Julie Lerman', 'c461183d-e3e4-4f14-a4d4-34431a3b4c39', '@julielerman');
 
